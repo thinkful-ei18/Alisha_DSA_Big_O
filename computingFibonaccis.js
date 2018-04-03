9aae2bb895866d355c0eb2343139a80dcaab731f
alishaevn <alieva.inc@gmail.com>
2018-04-03 09:25:49 -0700

    completed

---
+++ computingFibonaccis.js
@@ -0,0 +1,39 @@
+'use strict';
+
+function generateFib(num) {
+  let result = [];
+  for (let i = 1; i <= num; i++) {
+
+    // we're adding the first item
+    // to the result list, append the
+    // number 0 to results
+    if (i === 1) {
+      result.push(0);
+    }
+    // ...and if it's the second item
+    // append 1
+    else if (i == 2) {
+      result.push(1);
+    }
+
+    // otherwise, sum the two previous result items, and append that value to results.
+    else {
+      result.push(result[i - 2] + result[i - 3]);
+    }
+  }
+  // once the for loop finishes
+  // we return `result`.
+  return result;
+}
+
+/*
+O()
+
+- first operation: constant
+- second operation: linear. there are various if statements inside the loop, 
+  but only one will apply each time through the array
+- third operation: constant
+
+- the linear operation is the delay
+
+*/
